5567a5ba6590e35ebba8e1580cc4bed378141639
bipin-01 <poudel7700@gmail.com>
2023-03-21 22:07:22 +0545

    First commit and setup

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import appConfig from './config/app.config'
+import { LoggerModule } from 'nestjs-pino'
+import { CommonModule } from './common/common.module'
+@Module({
+    imports: [
+        CommonModule,
+        ConfigModule.forFeature(appConfig),
+        LoggerModule.forRoot({
+            pinoHttp: {
+                customProps: (req, res) => ({
+                    context: 'HTTP',
+                }),
+                transport: {
+                    target: 'pino-pretty',
+                    options: {
+                        singleLine: true,
+                        colorize: true
+                    },
+                },
+            },
+        })],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {}
